5bcb9eac34b60835d4ea743ac094b6be0f2c75ae
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-03-24 09:08:36 -0300

    style: finaliza estilização do componente de carregamento de telas

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Spinner, Box, Highlight } from '@chakra-ui/react';
 
 function Loading() {
   return (
-    <div>Loading</div>
+    <Box h="calc(100vh)" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+        mb="6px"
+      />
+      <Highlight
+        query="Carregando"
+        styles={{
+          px: '2', py: '1', rounded: 'full', bg: 'blue.200',
+        }}
+      >
+        Carregando
+      </Highlight>
+    </Box>
   );
 }
 
